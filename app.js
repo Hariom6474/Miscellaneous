c644cf468c431258022790db20a7787d3d509f38
hariom6474 <hariom6474@gmail.com>
2024-01-10 23:37:49 +0530

    Clean Up Code

--- app.js
+++ app.js
@@ -1,54 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-      if (err) {
-        console.error(err);
-        data = "";
-      }
-      res.write("<html>");
-      res.write("<head><title>Enter Message</title></head>");
-      res.write(`${data}`);
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  } else if (url === "/message" && method === "POST") {
-    const body = []; // to copy the data
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        if (err) {
-          console.error(err);
-        }
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    res.end();
-  }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(4000, () => {
   console.log("Server is running at http://localhost:4000/");
