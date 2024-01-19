0e498a4f4ca83217b2a2ccc0d38e5de33dc9a7b2
hariom6474 <hariom6474@gmail.com>
2024-01-20 00:43:39 +0530

    What the hell are Models?

--- New folder/routes/admin.js
+++ New folder/routes/admin.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const router = express.Router();
-const productsController = require('../controllers/products');
+const productsController = require("../controllers/products");
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post("/add-product", productsController.postAddProduct);
 
 module.exports = router;
